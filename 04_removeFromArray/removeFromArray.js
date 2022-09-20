84bb6be1e4bed13e76347fd81d71a92197e5c47b
Remy <remy@chaoticmewtral.com>
2022-09-20 18:23:59 -0400

    Create compare and filter functions in removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+let firstArray;
+let toRemove;
+
+const removeFromArray = function(item) {
+    return item !== toRemove;
 
 }
 
+function myFunction() {
+    let newArray = firstArray.filter(removeFromArray);
+}
 // Do not edit below this line
 module.exports = removeFromArray;
