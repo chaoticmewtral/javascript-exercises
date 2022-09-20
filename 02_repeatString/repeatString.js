8602ba1b69de4f548b1116cb82686fd8acc1b2c8
Remy <remy@chaoticmewtral.com>
2022-09-20 12:48:28 -0400

    Add ERROR return for negative inputs in repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,14 @@
 const repeatString = function(string, num) {
 let i = num;
 let str = "";
-for (let i = num; i >= 0; --i) {
-str += string;
+while (i) {
+    if (i < 0) {
+        str = 'ERROR';
+    }  else {
+    str += string;
+    i--;
+    }
 }
-// while (i) {
-//     str += string;
-//     i--;
-// }
 return str;
 };
 
