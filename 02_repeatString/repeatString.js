484de46799ea9e924e2f27115b0efdda9660d00f
Remy <remy@chaoticmewtral.com>
2022-09-20 13:01:19 -0400

    Move if to outside of for loop in repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,15 +1,16 @@
 const repeatString = function(string, num) {
 let i = num;
 let str = "";
-for (; i > 0; ) {
-    if (i < 0) {
-        str = 'ERROR';
-    }  else {
-    str += string;
-    i--;
+
+if (i < 0) {
+    return "ERROR";
+} else {
+    for (; i > 0; ) {
+        str += string;
+        i--;
     }
+    return str;
 }
-return str;
 };
 
 // Do not edit below this line
