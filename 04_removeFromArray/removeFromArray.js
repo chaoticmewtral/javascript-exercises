850c52c9ea3083b57e81a723c64b9d3cc60b519a
Remy <remy@chaoticmewtral.com>
2022-09-22 13:42:03 -0400

    Reset removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,6 @@
-// create a function that compares the given value (rmv) to each array item
-// convert array to string?
-
-// declare input variables
-let value;
-let arr;
-
-// assign input variables to function
 const removeFromArray = function(arr, value) {
-   let index = arr.indexOf(value);
-   arr.splice(index);
-   return arr;
+
    }
+
 // Do not edit below this line
 module.exports = removeFromArray;
