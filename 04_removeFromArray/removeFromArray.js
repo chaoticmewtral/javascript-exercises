5bb9e313b7fdc464c80caf3a7f9510ab1feb8266
Remy <remy@chaoticmewtral.com>
2022-09-22 16:49:01 -0400

    Add to removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,14 +2,12 @@ const removeFromArray = function() {
     let arr = arguments[0]; // given array
     let newArr = [];
     for (let i = 1; i < arguments.length; i++) {
-        for (let item of arr) {
-            if item !== arguments[i] {
-                newArr.push(item);
-            }
+        if arr.includes(argument[i]) {
+            newArr.push(arr.splice(arr.indexOf(argument[i]), 1));
         }
+        return newArr;
     }
-    return newArr;
-   }
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
