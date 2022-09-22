b35738b301986353322e58c97f2157abbae5545c
Remy <remy@chaoticmewtral.com>
2022-09-22 17:57:13 -0400

    Place missing } in removefromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,14 +3,15 @@ const removeFromArray = function() {
     let newArr = [];
     for (let i = 1; i < arguments.length; i++) {
         let val = arguments[i];
-        newArr = arr.filter(comp);
+        newArr = arr.push(arr.filter(comp));
 
         function comp(itm) {
             if (itm !== val) {
                 return itm;
+            }
         }
-        return newArr;
     }
+    return newArr;
 }
 
 // Do not edit below this line
