c3bac7c3c0a396c92c321920defd8b31dba7ff7c
Remy <remy@chaoticmewtral.com>
2022-09-22 16:23:21 -0400

    Add to removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    let arr = arguments[0]; // given array
+    let newArr;
+    for (let i = 1; i < arguments.length; i++) {
+        for (let item of arr) {
+            if item !== arguments[i] {
+                newArr.push(item);
+            }
+        }
+    }
+    return newArr;
    }
 
 // Do not edit below this line
