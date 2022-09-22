1eda96f81924f367cf5c775a293b51d5250cfb14
Remy <remy@chaoticmewtral.com>
2022-09-22 16:51:31 -0400

    Added missing () to if in removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function() {
     let arr = arguments[0]; // given array
     let newArr = [];
     for (let i = 1; i < arguments.length; i++) {
-        if arr.includes(argument[i]) {
+        if (arr.includes(argument[i])) {
             newArr.push(arr.splice(arr.indexOf(argument[i]), 1));
         }
         return newArr;
