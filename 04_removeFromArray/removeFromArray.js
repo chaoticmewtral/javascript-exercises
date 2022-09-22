c9d63070d3607815e9c0f1b27eb8f22cf68d53da
Remy <remy@chaoticmewtral.com>
2022-09-22 16:57:32 -0400

    Fix typo of val in removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,7 +3,7 @@ const removeFromArray = function() {
     let newArr = [];
     for (let i = 1; i < arguments.length; i++) {
         let val = arguments[i];
-        if (arr.includes(value)) {
+        if (arr.includes(val)) {
             newArr.push(arr.splice(arr.indexOf(val), 1));
         }
         return newArr;
