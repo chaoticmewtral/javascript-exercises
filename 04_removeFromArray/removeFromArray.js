1c14a5a783fecb602c3a7d6b50bb97e79a7410ca
Remy <remy@chaoticmewtral.com>
2022-09-22 16:56:41 -0400

    Fix typo of arguments in removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function() {
     let arr = arguments[0]; // given array
     let newArr = [];
     for (let i = 1; i < arguments.length; i++) {
-        let val = argument[i];
+        let val = arguments[i];
         if (arr.includes(value)) {
             newArr.push(arr.splice(arr.indexOf(val), 1));
         }
