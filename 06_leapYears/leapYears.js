66458f9ee3c33d22a20c7efae8d4b59d55188923
Remy <remy@chaoticmewtral.com>
2022-09-23 15:15:22 -0400

    Add = to else if to make comparison operator in leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,7 @@
 const leapYears = function(year) {
     if ((year % 4) != 0) {
         return false;
-    } else if (((year % 100) = 0) && ((year % 400) != 0)) {
+    } else if ((year % 100 == 0) && ((year % 400) != 0)) {
         return false;
     } else {
         return true;
