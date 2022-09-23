6e62e6e0b6b766e323976f8b88f08ca71343c3e5
Remy <remy@chaoticmewtral.com>
2022-09-23 15:08:15 -0400

    Add to leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,14 +1,13 @@
 const leapYears = function(year) {
     if ((year % 4) != 0) {
-        return "false";
+        return false;
+    } else if ((year % 100 = 0) && (year % 400 != 0)) {
+        return false;
     } else {
-        if ((year % 400) != 0) {
-            return "false";
-        } else {
-            return "true";
-        }
+        return true;
     }
 }
+
 
 // Do not edit below this line
 module.exports = leapYears;
