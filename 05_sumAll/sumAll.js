5d7572c1e8e6fd3ff766609f581f842b4f615525
Remy <remy@chaoticmewtral.com>
2022-09-23 12:29:48 -0400

    Reset function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,27 +1,21 @@
-// const sumAll = function(start, end) {
-//     let sumTotal = 0;
-//     for (let i = start; i <= end; i++) {
-//             sumTotal += i;
-//     }
-// return sumTotal;
-// };
+/*
+const sumAll = function(start, end) {
+    let sumTotal = 0;
+    for (let i = start; i <= end; i++) {
+            sumTotal += i;
+    }
+return sumTotal;
+};
+*/
 
 // pass first test; comment above and copy code for skip edits
 // Edit for skipped tests
 
 const sumAll = function(...args) {
-    let sumTotal = 0;
-    let start, end;
+    
+    return sumTotal;
+}
 
-    for (const arg of args) {
-        if (typeof arg === "number" && typeof arg >= 0) {
-            for (let i = start; i <= end; i++) {
-                sumTotal += i;
-            }
-        }
-    }
-return sumTotal;
-};
 
 // Do not edit below this line
 module.exports = sumAll;
