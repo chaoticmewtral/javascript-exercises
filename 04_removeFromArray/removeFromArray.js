ba74b9e5eda81fd5509755d5715c19a5b11a8ef5
Remy <remy@chaoticmewtral.com>
2022-09-23 09:22:41 -0400

    Start over on removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,5 @@
 const removeFromArray = function() {
-    let arr = arguments[0]; // given array
-    let newArr = [];
-    for (let i = 1; i < (arguments.length - 1); i++) {
-        let val = arguments[i];
-        let itm = arr[""];
 
-        function comp(itm) {
-            if (itm !== val) {
-                return itm;
-            }
-        }
-        newArr.push(comp());
-    }
-    return newArr;
 }
 
 // Do not edit below this line
