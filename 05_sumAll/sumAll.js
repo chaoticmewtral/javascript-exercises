2717d25b01a63e39b1caca2706c61f8c9e009a3c
Remy <remy@chaoticmewtral.com>
2022-09-23 14:09:40 -0400

    Add to sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -12,7 +12,29 @@ return sumTotal;
 // Edit for skipped tests
 
 const sumAll = function(...args) {
- 
+    let totalSum = "";
+
+    for (let arg of args) {
+        if ((typeof arg === "number") && (arg >= 0)) {
+            let start;
+            let end;
+
+            if (args[0] > args[1]) {
+                start = args[1];
+                end = args[0];
+            } else {
+                start = args[0];
+                end = args[1];
+            }
+
+            for (let i = start; i <= end; i++) {
+                totalSum += i;
+            }
+        } else {
+            totalSum = "ERROR";
+        }
+    }
+    return totalSum;
 }
 
 
