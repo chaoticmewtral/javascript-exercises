8d8e9e9715008160fc4407620201da6efdd87b7f
Remy <remy@chaoticmewtral.com>
2022-09-23 09:28:49 -0400

    Use rest operator for removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+let array = args[0];
+let newArray = [];
 
-}
+array.forEach((item) => {
+    if (!args.includes(item)) {
+        newArray.push(item);
+    }
+});
+return newArray;
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
