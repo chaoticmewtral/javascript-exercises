f03201657a896084314268c11fa880e803c78f41
Remy <remy@chaoticmewtral.com>
2022-09-23 13:40:03 -0400

    Reset sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -12,27 +12,7 @@ return sumTotal;
 // Edit for skipped tests
 
 const sumAll = function(...args) {
-    const arr = args;
-    let sumTotal;
-
-    arr.forEach((arg) => {
-        if ((typeof arg === "number") && (arg >= 0)) {
-            let start, end;
-            if (arr[0] < arr[1]) {
-                start = arr[0];
-                end = arr[1];
-            } else {
-                start = arr[1];
-                end = arr[0];
-            }
-            for (let i = start; i <= end; i++) {
-                sumTotal += i;
-            }
-        } else {
-            sumTotal = "ERROR";
-        }
-    });
-    return sumTotal;
+ 
 }
 
 
