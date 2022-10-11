321dab140bdd7c895bb2161c643d7e485ba85b9f
Remy <remy@chaoticmewtral.com>
2022-10-11 15:42:40 -0400

    Add to palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+// get charCodeAt each character in str using loop
+let arr = [];
 
+for (let i = 0; i < str.length; i++) {
+    let code = str.charCodeAt(i);
+
+// if charCode is a letter, add it to array
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+        arr.push(str.charAt(i));
+    }
+}
+    
+// function to compare if str/arr === rev of str/arr
 };
 
 // Do not edit below this line
