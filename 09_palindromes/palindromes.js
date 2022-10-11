2dec1efcd366e826104d2bc6ccc8fdb5f9fa6659
Remy <remy@chaoticmewtral.com>
2022-10-11 16:57:07 -0400

    Add to palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
 const palindromes = function (initStr) {
 // get charCodeAt each character in str using loop
 let arr = [];
+let rev = [];
 let str = initStr.toLowerCase();
 
 for (let i = 0; i < str.length; i++) {
@@ -9,11 +10,13 @@ for (let i = 0; i < str.length; i++) {
 // if charCode is a letter, add it to array
     if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
         arr.push(str.charAt(i));
+        rev.push(str.charAt(i));
     }
 }
-return (arr.toString() === (arr.reverse()).toString);
-// function to compare if str/arr === rev of str/arr
-};
+
+
+return (rev.reverse() === arr);
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+// module.exports = palindromes;
