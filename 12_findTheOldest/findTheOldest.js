cd44ec7642023d3b8a9b3936b407d65238a60c03
Remy <remy@chaoticmewtral.com>
2022-10-12 19:20:30 -0400

    Add to findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,20 +1,20 @@
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ];
+// const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1942,
+//       yearOfDeath: 1970,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+//   ];
   
   const findTheOldest = function() {
 
@@ -26,17 +26,14 @@ const people = [
             
         const age = person.yearOfDeath - person.yearOfBirth;
         person.age = age;
-        console.log(person);
         }
         if (acc === null || person.age > acc.age) {
             return person;
-        } else {
-            return acc;
         }
-
+        return acc;
     }, null);
-    return oldest;
+    console.log(oldest);
 };
 
 // Do not edit below this line
-// module.exports = findTheOldest;
+module.exports = findTheOldest;
