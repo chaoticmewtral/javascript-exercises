4e7629be698ae5226b6e94cb667a8ac05910f22f
Remy <remy@chaoticmewtral.com>
2022-10-12 15:34:58 -0400

    Add current year to function in findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
 const findTheOldest = function() {
-
+    if (people.yearOfDeath === undefined) {
+        people.yearOfDeath = 2022;
+        return(people.yearOfDeath - people.yearOfBirth);
+}
 };
 
 // Do not edit below this line
