157c210a4cdb6926b7b4770da992ee2ab55fa056
Remy <remy@chaoticmewtral.com>
2022-10-12 15:12:05 -0400

    Add to getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let bookArray = [];
 
+    for (let book in books) {
+        bookArray.push(book.title);
+    }
+    return bookArray;
 };
 
 // Do not edit below this line
