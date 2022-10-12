6fd45317da6623485baf520b6688c98e04c37f36
Remy <remy@chaoticmewtral.com>
2022-10-12 17:19:06 -0400

    Reset findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,6 @@
-const findTheOldest = function(people) {
-    for (let person of people) {
-        if (!"yearOfDeath" in person) {
-            person.yearOfDeath = 2022;
-        }
-    }
-    const oldest = people.sort(function(a, b) {
-        const lastPerson = a.yearOfDeath - a.yearOfBirth;
-        const nextPerson = b.yearOfDeath - b.yearOfBirth;
-        return lastPerson > nextPerson ? -1 : 1;
-    });
-    return oldest;
+const findTheOldest = function() {
+
 };
 
 // Do not edit below this line
-module.exports = findTheOldest;
+// module.exports = findTheOldest;
