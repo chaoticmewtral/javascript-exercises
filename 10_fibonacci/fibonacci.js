3066711b06ce3ce25a87d147be61eed2b1d5df93
Remy <remy@chaoticmewtral.com>
2022-10-12 13:37:44 -0400

    Add to fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,14 +1,15 @@
 const fibonacci = function(pos) {
     let fibArray = [1];
     const posInt = parseInt(pos);
-    let fibNum;
+    let fibNum = 0;
     let fibInd = (posInt + 1);
 
     if (posInt <= 0) {
         fibNum = "OOPS";
     } else {
         for(let i = 0; i < posInt; i++) {
-
+            fibNum = fibNum + fibArray[i];
+            fibArray.push(fibNum);
         }
     }
 return posInt[fibInd];
