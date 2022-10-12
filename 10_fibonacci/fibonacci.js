4cce826397597d175e07f123d28f6466e7efcc25
Remy <remy@chaoticmewtral.com>
2022-10-12 13:55:33 -0400

    Restart fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,19 +1,6 @@
-const fibonacci = function(pos) {
-    let fibArray = [1];
-    const posInt = parseInt(pos);
-    let fibNum = 0;
-    let fibInd = (posInt + 1);
+const fibonacci = function(position) {
 
-    if (posInt <= 0) {
-        fibNum = "OOPS";
-    } else {
-        for(let i = 0; i < posInt; i++) {
-            fibNum = fibNum + fibArray[i];
-            fibArray.push(fibNum);
-        }
-    }
-return posInt[fibInd];
 };
 
 // Do not edit below this line
-// module.exports = fibonacci;
+module.exports = fibonacci;
