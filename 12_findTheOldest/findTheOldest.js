ca64c266567dfcf8e8965736544af20f80d9c488
Remy <remy@chaoticmewtral.com>
2022-10-12 19:13:25 -0400

    Add to oldest const in findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,42 @@
-const findTheOldest = function() {
-    for (let person of people) {
-        if (!'yearOfDeath' in person) {
-            person.yearOfDeath = 2022;
-        }
-    const age = person.yearOfDeath - person.yearOfBirth;
-    }
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ];
+  
+  const findTheOldest = function() {
+
     const oldest = people.reduce((acc, person) => {
-        if (acc === null || person.age < acc.age) {
+        for (let person of people) {
+            if (!'yearOfDeath' in person) {
+                person.yearOfDeath = 2022;
+            }
+            
+        const age = person.yearOfDeath - person.yearOfBirth;
+        person.age = age;
+        console.log(person);
+        }
+        if (acc === null || person.age > acc.age) {
             return person;
         } else {
             return acc;
         }
+
     }, null);
     return oldest;
 };
 
 // Do not edit below this line
-module.exports = findTheOldest;
+// module.exports = findTheOldest;
