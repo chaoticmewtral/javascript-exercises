8b33c548baed6646dfc392b4f635cee27c839279
Remy <remy@chaoticmewtral.com>
2022-10-13 11:36:40 -0400

    Reset code in findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -16,22 +16,7 @@ const people = [
   ]
   
   const findTheOldest = function() {
-    for (let person of people) {
-        if (!'yearOfDeath' in person) {
-            let year = new Date().getFullYear;
-            person["yearOfDeath"] = year;
-        }
-    }
-    const oldest = people.reduce((acc, person) => {
-        console.log(person.yearOfDeath);
-        const age = person.yearOfDeath - person.yearOfBirth;
-        person["age"] = age;
-        if (acc === null || person.age > acc.age) {
-            return person;
-        }
-        return acc;
-    }, null);
-return oldest;
+
 };
 
 // passed first test above this line
