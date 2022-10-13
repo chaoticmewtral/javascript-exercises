cdbc334b61bb809a89ddd48629b8ec388e1dc3a1
Remy <remy@chaoticmewtral.com>
2022-10-13 10:24:08 -0400

    Add to findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -16,7 +16,7 @@
 //     },
 //   ];
   
-  const findTheOldest = function() {
+  const findTheOldest = function(people) {
 
     const oldest = people.reduce((acc, person) => {
         for (let person of people) {
@@ -32,7 +32,7 @@
         }
         return acc;
     }, null);
-
+return oldest;
 };
 
 // Do not edit below this line
